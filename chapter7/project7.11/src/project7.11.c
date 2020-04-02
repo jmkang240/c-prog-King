3e44f1005a6d571f229aa591c14030a49bce6eb4
jmkang240 <jmkang240@gmail.com>
2020-04-01 20:10:27 -0600

    Have to remove repo to empty out Project Explorer

---
+++ chapter7/project7.11/src/project7.11.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : 11.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Looks like Im going to have to do this the hard way!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}

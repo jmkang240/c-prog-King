e260e36e22294ca10e290f6b5887ec34a12fdf78
jmkang240 <jmkang240@gmail.com>
2020-05-01 11:11:00 -0600

    8.12edit

--- chapter8/project8.12/src/project8.12.c
+++ chapter8/project8.12/src/project8.12.c
@@ -15,12 +15,12 @@
 #define MAX_SCRABBLE_LEN 26
 
 int main(void) {
-								   /* A, B, C, D, E, F, G, H, I, J */
+                                   /* A, B, C, D, E, F, G, H, I, J */
 	int scrabble[MAX_SCRABBLE_LEN] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8,
-			                       /* K, L, M, N, O, P,  Q, R, S, T */
-			                          5, 1, 3, 1, 1, 3, 10, 1, 1, 1,
-								   /* U, V, W, X, Y, Z */
-									  1, 4, 4, 8, 4, 10 };
+                                   /* K, L, M, N, O, P,  Q, R, S, T */
+                                      5, 1, 3, 1, 1, 3, 10, 1, 1, 1,
+                                   /* U, V, W, X, Y, Z */
+                                      1, 4, 4, 8, 4, 10 };
 	char letter;
 	int val=0;
 
